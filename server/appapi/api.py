6ee23a1f8425618ac54e4406ce36500c343822b9
Shuoni Xu <s358xu@uwaterloo.ca>
2021-12-02 01:41:27 +0800

    Update template json

--- server/appapi/api.py
+++ server/appapi/api.py
@@ -7,10 +7,12 @@ Created by Xiong, Kaijie on 2021-11-24.
 Copyright © 2021 Xiong, Kaijie & Xu, Shuoni. All rights reserved.
 """
 
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, requests
 import random
+import requests
 
 from .models import db, Card
+<<<<<<< Updated upstream
 from .apibox import getWeatherData
 from .volvo_connected_api import getVehicleModel
 from .volvo_connected_api import getWindowStatus
@@ -34,6 +36,11 @@ from .volvo_connected_api import postHonk
 from .volvo_connected_api import postLock
 from .volvo_connected_api import postUnlock
 from .volvo_connected_api import postNavigation
+=======
+from .apibox import getWeatherData, getDateTime
+from .volvo_connected_api import getVehicleModel, getWindowStatus, getWarnings, getTyreStatus, getVehicleStat, getOdometer, getFuel, getEnvironment, getEngineDiagnostics, getDoorLock, getDiagnostic, getBrakeStatus
+from .volvo_connected_api import postClimatizationStart, postClimatizationStop, postEngineStart, postEngineStop, postFlash, postHonkFlash, postHonk, postLock, postUnlock, postNavigation
+>>>>>>> Stashed changes
 
 
 api = Blueprint('api', __name__)
@@ -137,7 +144,64 @@ def getBrake():
     result = getBrakeStatus()
     return jsonify(result)
 
+<<<<<<< Updated upstream
 @api.route('/', methods = ['GET'])
 def getBrake():
     result = getBrakeStatus()
     return jsonify(result)
+=======
+
+@api.route('/post-climatization-start/', methods = ['POST'])
+def postClimStart():
+    status = postClimatizationStart()
+    return status
+
+
+@api.route('/post-climatization-stop', methods = ['POST'])
+def postClimStop():
+    status = postClimatizationStop()
+    return status
+
+
+@api.route('/post-engine-start', methods = ['POST'])
+def postEngStart():
+    status = postEngineStart()
+    return status
+
+
+@api.route('/post-engine-stop', methods = ['POST'])
+def postEngStop():
+    status = postEngineStop()
+    return status
+
+
+@api.route('/post-flash', methods = ['POST'])
+def postFla():
+    status = postFlash()
+    return status
+
+
+@api.route('/post-honkflash', methods = ['POST'])
+def postHonFla():
+    status = postHonkFlash()
+    return status
+
+
+@api.route('/post-honk', methods = ['POST'])
+def postHo():
+    status = postHonk()
+    return status
+
+
+@api.route('/post-lock', methods = ['POST'])
+def postLo():
+    status = postLock()
+    return status
+
+
+@api.route('/post-navigation', methods = ['POST'])
+def postNavi():
+    status = postNavigation()
+    return status
+
+>>>>>>> Stashed changes
--- server/appapi/volvo_extended_api.py
+++ server/appapi/volvo_extended_api.py
@@ -5,3 +5,78 @@ volvoapi.py
 Created by Xiong, Kaijie on 2021-11-26.
 Copyright © 2021 Xiong, Kaijie & Xu, Shuoni. All rights reserved.
 """
+
+from flask import Blueprint, jsonify
+from flask.json import dumps
+import requests
+import json
+from datetime import datetime
+
+
+def get_avfuel_consum():
+    avfuel_url = "https://api.volvocars.com/extended-vehicle/v1/vehicles/YV4952NA4F120DEMO/resources/averageFuelConsumption"
+    res = requests.get(avfuel_url, headers = "")
+
+    a_data = res.json()
+
+    return a_data
+
+def get_av_speed():
+    avspeed_url = ""
+    res = requests.get(avspeed_url, headers = "")
+
+    a_data = res.json()
+
+    return a_data
+
+def get_battery_remaining():
+    batteryremain_url = ""
+    res = requests.get(batteryremain_url, headers = "")
+
+    b_data = res.json()
+
+    return b_data
+
+def get_brakefluid():
+    brakefluid_url = ""
+    res = requests.get(brakefluid_url, headers = "")
+
+    b_data = res.json()
+
+    return b_data
+
+def get_bulb_failure():
+    bulbfailure_url = ""
+    res = requests.get(bulbfailure_url, headers = "")
+
+    b_data = res.json()
+
+    return b_data
+
+# def distance_to_empty():
+
+def get_enginecoollevel():
+    enginecoollevel_url = ""
+    res = requests.get(enginecoollevel_url, headers = "")
+
+    e_data = res.json()
+
+    return e_data
+
+def get_enginecooltemp():
+    enginecooltemp_url = ""
+    res = requests.get(enginecooltemp_url, headers = "")
+
+    e_data = res.json()
+
+    return e_data
+
+def get_engineserhr():
+    engineserhr_url = ""
+    res = requests.get(engineserhr_url, headers = "")
+
+    e_data = res.json()
+
+    return e_data
+
+def
