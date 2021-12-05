d7f96116ff8d6c87b47c3abb5423939a0d0cc363
Shuoni Xu <s358xu@uwaterloo.ca>
2021-12-06 01:33:48 +0800

    Update volvo_extended_api.py

--- server/appapi/volvo_extended_api.py
+++ server/appapi/volvo_extended_api.py
@@ -9,6 +9,7 @@ Copyright © 2021 Xiong, Kaijie & Xu, Shuoni. All rights reserved.
 from flask import Blueprint
 import requests
 from .config import CredentialConfig
+import json
 
 extended_apibox = Blueprint('extended_apibox', __name__)
 
@@ -19,9 +20,25 @@ headersInfo['authorization'] = getattr(credentialInfo, 'token')
 headersInfo['vcc-api-key'] = getattr(credentialInfo, 'vcc_api_key')
 
 
+class VehicleVin():
+    # get Vin and return builded url
+    def getVehicleVinUrl():
+        url = 'https://api.volvocars.com/extended-vehicle/v1/vehicles'
+
+        print(headersInfo)
+
+        r = requests.get(url, headers = headersInfo)
+        raw_data = r.json()
+        print(json.dumps(raw_data, indent=4, sort_keys=True))
+
+        vehicleVin = raw_data['data'][0]['vin']
+        vehicleUrl = 'https://api.volvocars.com/extended-vehicle/v1/vehicles/' + vehicleVin
+
+        return vehicleUrl
+
 class ExtendedApi():
     def get_avfuel_consum():
-        avfuel_url = "https://api.volvocars.com/extended-vehicle/v1/vehicles/YV4952NA4F120DEMO/resources/averageFuelConsumption"
+        avfuel_url = VehicleVin.getVehicleVinUrl() + "/resources/averageFuelConsumption"
         res = requests.get(avfuel_url, headers = headersInfo)
 
         a_data = res.json()
@@ -30,7 +47,7 @@ class ExtendedApi():
 
 
     def get_av_speed():
-        avspeed_url = ""
+        avspeed_url = VehicleVin.getVehicleVinUrl() + "/resources/averageSpeed"
         res = requests.get(avspeed_url, headers = headersInfo)
 
         a_data = res.json()
@@ -39,7 +56,7 @@ class ExtendedApi():
 
 
     def get_battery_remaining():
-        batteryremain_url = ""
+        batteryremain_url = VehicleVin.getVehicleVinUrl() + "/resources/backupBatteryRemaining"
         res = requests.get(batteryremain_url, headers = headersInfo)
 
         b_data = res.json()
@@ -48,7 +65,7 @@ class ExtendedApi():
 
 
     def get_brakefluid():
-        brakefluid_url = ""
+        brakefluid_url = VehicleVin.getVehicleVinUrl() + "/resources/brakeFluid"
         res = requests.get(brakefluid_url, headers = headersInfo)
 
         b_data = res.json()
@@ -57,7 +74,7 @@ class ExtendedApi():
 
 
     def get_bulb_failure():
-        bulbfailure_url = ""
+        bulbfailure_url = VehicleVin.getVehicleVinUrl() + "/resources/bulbFailure"
         res = requests.get(bulbfailure_url, headers = headersInfo)
 
         b_data = res.json()
@@ -66,7 +83,7 @@ class ExtendedApi():
 
 
     def get_enginecoollevel():
-        enginecoollevel_url = ""
+        enginecoollevel_url = VehicleVin.getVehicleVinUrl() + "/resources/engineCoolantLevel"
         res = requests.get(enginecoollevel_url, headers = headersInfo)
 
         e_data = res.json()
@@ -75,7 +92,7 @@ class ExtendedApi():
 
 
     def get_enginecooltemp():
-        enginecooltemp_url = ""
+        enginecooltemp_url = VehicleVin.getVehicleVinUrl() + "/resources/engineCoolantTemp"
         res = requests.get(enginecooltemp_url, headers = headersInfo)
 
         e_data = res.json()
@@ -84,7 +101,7 @@ class ExtendedApi():
 
 
     def get_engineserhr():
-        engineserhr_url = ""
+        engineserhr_url = VehicleVin.getVehicleVinUrl() + "/resources/engineHoursToService"
         res = requests.get(engineserhr_url, headers = headersInfo)
 
         e_data = res.json()
@@ -92,7 +109,7 @@ class ExtendedApi():
         return e_data
 
     def get_trip_meter1():
-        tripmeter_url = ""
+        tripmeter_url = VehicleVin.getVehicleVinUrl() + "/resources/tripMeter1"
         res = requests.get(tripmeter_url, headers=headersInfo)
 
         t_data = res.json()
@@ -100,7 +117,7 @@ class ExtendedApi():
         return t_data
 
     def get_trip_meter2():
-        tripmeter_url = ""
+        tripmeter_url = VehicleVin.getVehicleVinUrl() + "/resources/tripMeter2"
         res = requests.get(tripmeter_url, headers=headersInfo)
 
         t_data = res.json()
@@ -108,18 +125,9 @@ class ExtendedApi():
         return t_data
 
     def get_odometer():
-        odometer_url = ""
+        odometer_url = VehicleVin.getVehicleVinUrl() + "/resources/odometer"
         res = requests.get(odometer_url, headers=headersInfo)
 
         o_data = res.json()
 
         return o_data
-
-    def get_frontleft_tyre():
-        frontleft_tyre_url = ""
-        res = requests.get(frontleft_tyre_url, headers=headersInfo)
-
-        f_data = res.json()
-
-        return f_data
-
