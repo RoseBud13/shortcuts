037b621446da02f0af51f26d817db22832137bbb
RoseBud13 <xiongkaijie13@gmail.com>
2021-12-05 19:01:09 +0800

    add templates parser and modified others

--- server/appapi/api.py
+++ server/appapi/api.py
@@ -11,19 +11,31 @@ from flask import Blueprint, jsonify
 import random
 import requests
 
-from .models import db, Card
-from .apibox import getWeatherData, getDateTime
-from .volvo_connected_api import getVehicleModel, getWindowStatus, getWarnings, getTyreStatus, getVehicleStat, getOdometer, getFuel, getEnvironment, getEngineDiagnostics, getDoorLock, getDiagnostic, getBrakeStatus
-from .volvo_connected_api import postClimatizationStart, postClimatizationStop, postEngineStart, postFlash, postHonkFlash, postHonk, postLock, postUnlock, postNavigation, postEngineStop
+from .template_parser import run, getAllFileNames
+
+from .apibox import Apibox
+from .volvo_connected_api import ConnectedApi
 
 
 api = Blueprint('api', __name__)
 
 
+@api.route('/test/', methods=['GET'])
+def testImport():
+    r = run()
+    return 'test'
+
+
+@api.route('/get-templates/', methods=['GET'])
+def getTemplates():
+    data = getAllFileNames()
+    return jsonify({'templates': data})
+
+
 @api.route('/cards-info/', methods=['GET'])
-def getCardsInfo():
-    cards = Card.query.all()
-    return jsonify({'cardsInfo': [card.to_dict() for card in cards]})
+def getCards():
+    data = Apibox.getCardsInfo()
+    return jsonify(data)
 
 
 # for example
@@ -37,139 +49,139 @@ def getCar():
 
 @api.route('/get-datetime/', methods=['GET'])
 def getDateInfo():
-    result = getDateTime()
+    result = Apibox.getDateTime()
     return jsonify({'dateInfo': result})
 
 
 @api.route('/get-weather/', methods=['GET'])
 def getWeather():
-    result = getWeatherData()
+    result = Apibox.getWeatherData()
     return jsonify(result)
 
 
 # for example
 @api.route('/get-v-model/', methods=['GET'])
 def getVModel():
-    result = getVehicleModel()
+    result = ConnectedApi.getVehicleModel()
     return result
 
 
 @api.route('/get-windows-status/', methods = ['GET'])
 def getWinStatus():
-    result = getWindowStatus()
+    result = ConnectedApi.getWindowStatus()
     return jsonify(result)
 
 
 @api.route('/get-warning/', methods = ['GET'])
 def getWarnStatus():
-    result = getWarnings()
+    result = ConnectedApi.getWarnings()
     return jsonify(result)
 
 
 @api.route('/get-tyre/', methods = ['GET'])
 def getTyre():
-    result = getTyreStatus()
+    result = ConnectedApi.getTyreStatus()
     return jsonify(result)
 
 
 @api.route('/get-v-stat/', methods = ['GET'])
 def getStat():
-    result = getVehicleStat()
+    result = ConnectedApi.getVehicleStat()
     return jsonify(result)
 
 
 @api.route('/get-odo/', methods = ['GET'])
 def getOdo():
-    result = getOdometer()
+    result = ConnectedApi.getOdometer()
     return jsonify(result)
 
 
 @api.route('/get-fuel-info/', methods = ['GET'])
 def getFuelinfo():
-    result = getFuel()
+    result = ConnectedApi.getFuel()
     return jsonify(result)
 
 
 @api.route('/get-env-temp/', methods = ['GET'])
 def getEnvir():
-    result = getEnvironment()
+    result = ConnectedApi.getEnvironment()
     return jsonify(result)
 
 
 @api.route('/get-eng-diag/', methods = ['GET'])
 def getEngDiag():
-    result = getEngineDiagnostics()
+    result = ConnectedApi.getEngineDiagnostics()
     return jsonify(result)
 
 
 @api.route('/get-door-lock-status/', methods = ['GET'])
 def getLockStatus():
-    result = getDoorLock()
+    result = ConnectedApi.getDoorLock()
     return jsonify(result)
 
 
 @api.route('/get-v-diag/', methods = ['GET'])
 def getDiag():
-    result = getDiagnostic()
+    result = ConnectedApi.getDiagnostic()
     return jsonify(result)
 
 
 @api.route('/get-brake-status/', methods = ['GET'])
 def getBrake():
-    result = getBrakeStatus()
+    result = ConnectedApi.getBrakeStatus()
     return jsonify(result)
 
 
 @api.route('/post-climatization-start/', methods = ['POST'])
 def postClimStart():
-    status = postClimatizationStart()
+    status = ConnectedApi.postClimatizationStart()
     return status
 
 
 @api.route('/post-climatization-stop', methods = ['POST'])
 def postClimStop():
-    status = postClimatizationStop()
+    status = ConnectedApi.postClimatizationStop()
     return status
 
 
 @api.route('/post-engine-start', methods = ['POST'])
 def postEngStart():
-    status = postEngineStart()
+    status = ConnectedApi.postEngineStart()
     return status
 
 
 @api.route('/post-engine-stop', methods = ['POST'])
 def postEngStop():
-    status = postEngineStop()
+    status = ConnectedApi.postEngineStop()
     return status
 
 
 @api.route('/post-flash', methods = ['POST'])
 def postFla():
-    status = postFlash()
+    status = ConnectedApi.postFlash()
     return status
 
 
 @api.route('/post-honkflash', methods = ['POST'])
 def postHonFla():
-    status = postHonkFlash()
+    status = ConnectedApi.postHonkFlash()
     return status
 
 
 @api.route('/post-honk', methods = ['POST'])
 def postHo():
-    status = postHonk()
+    status = ConnectedApi.postHonk()
     return status
 
 
 @api.route('/post-lock', methods = ['POST'])
 def postLo():
-    status = postLock()
+    status = ConnectedApi.postLock()
     return status
 
 
 @api.route('/post-navigation', methods = ['POST'])
 def postNavi():
-    status = postNavigation()
+    status = ConnectedApi.postNavigation()
     return status
 
---
+++ server/appapi/template_parser.py
@@ -0,0 +1,102 @@
+"""
+data_prcss.py
+- fetch data from stored files and process them
+CI Hub server
+
+Created by Xiong, Kaijie on 2021-09-03.
+Copyright © 2021 Volvo Car Corporation. All rights reserved.
+"""
+
+import os
+import json
+from flask import Blueprint
+
+from .apibox import Apibox
+from .volvo_connected_api import ConnectedApi
+from .volvo_extended_api import ExtendedApi
+
+'''
+    Step1: read json
+    Step2: parse json to get functions
+    Step3: execute fucntions
+'''
+
+template_parser = Blueprint('template_parser', __name__)
+
+file_list = []
+
+def run():
+    data_dict = readStoredData('test.json')
+    print(data_dict)
+    function_list = get_template_function(data_dict)
+    exec_function(function_list)
+
+
+def getAllFileNames():
+
+    temp_file_list = []
+    basedir = os.path.abspath(os.getcwd())
+    file_path = os.path.join(basedir, 'templates/')
+
+    for f in os.listdir(file_path):
+        if not f.startswith('.'):
+            temp_file_list.append(f)
+    
+    file_list = sorted(temp_file_list)
+    print(file_list)
+    return file_list
+
+
+def readStoredData(target_file):
+
+    filename = target_file
+
+    basedir = os.path.abspath(os.getcwd()) # this path is for flask app
+    # basedir = os.path.dirname(os.getcwd())
+    file_path = os.path.join(basedir, 'templates/%s' % filename)
+
+    with open(file_path, 'r') as f:
+        data = json.load(f)
+    
+    return data
+
+
+def get_template_name(data_dict):
+    return data_dict['template_name']
+
+
+def get_template_function(data_dict):
+    function_list = []
+
+    for index in range(len(data_dict['step'])):
+        function_dict = data_dict['step'][index]
+        for i in function_dict:
+            function_list.append(function_dict[i]['function'])
+    print(function_list)
+    return function_list
+
+
+def exec_function(funtion_list):
+    for i in funtion_list:
+        if getattr(Apibox, i, False):
+            getattr(Apibox, i, False)()
+        elif getattr(ExtendedApi, i, False):
+            getattr(ExtendedApi, i, False)()
+        elif getattr(ConnectedApi, i, False):
+            getattr(ConnectedApi, i, False)()
+        else:
+            return False
+
+
+# if __name__ == '__main__':
+#     list = getAllFileNames()
+#     for i in list:
+#         data_dict = readStoredData(i)
+#         print(data_dict)
+#         print(get_template_name(data_dict))
+#         get_template_function(data_dict)
+#         function_list = get_template_function(data_dict)
+#         exec_function(function_list)
+
+
+
